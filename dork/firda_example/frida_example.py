ad97d988a23f7b5a7451545e1921bc5304bb68fa
prgrmz01@163.com <prgrmz01@163.com>
2022-12-06 15:49:01 +0800

    dev: 不知道函数地址,  但估计正常调起.exe了 并attach了

---
+++ dork/firda_example/frida_example.py
@@ -0,0 +1,46 @@
+import sys
+
+from frida import core
+from frida_tools.tracer import TracerApplication
+
+#让frida-trace能单独被调用运行
+# https://gitcode.net/fridaz/frida/-/commit/6ae5d35301c5d299a9a0f18077bb4a93801aef2c
+# https://gitcode.net/fridaz/frida-tools/-/commit/83cfbad68ec38fb98999ae50699c216b633dc9cd
+
+'''
+where frida-trace
+#D:/Python38/Scripts/frida-trace.exe
+
+frida-trace   -i "!"  -f  dork.exe
+'''
+# sys.argv=['D:\\Python38\\Scripts\\frida-trace.exe', '-i', '!', '-f', 'dork.exe']
+# sys.argv=['xxxx', '-i', '!', '-f', 'dork.exe']
+#
+# app = TracerApplication()
+# app.run()
+
+
+
+# from __future__ import print_function
+import frida
+import sys
+
+#device = frida.get_device_manager().enumerate_devices()
+local:core.Device = frida.get_local_device()
+# a = local.get_process('hello.exe')
+# print(a)
+pid:int = local.spawn("dork.exe")
+session = local.attach(pid)
+script = session.create_script("""
+Interceptor.attach(ptr("%s"), {
+    onEnter: function(args) {
+        send(args[0].toInt32());
+    }
+});
+""" % int('00000001400011D1', 16))
+# start .text 00000001400011D1 00000005   R . . . . . .
+def on_message(message, data):
+    print(message)
+script.on('message', on_message)
+script.load()
+sys.stdin.read()
