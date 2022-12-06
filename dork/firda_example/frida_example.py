8147f8b6ca3c779144b51a7d6901ec2f6f602018
prgrmz01@163.com <prgrmz01@163.com>
2022-12-06 16:08:00 +0800

    从文件读取脚本

--- dork/firda_example/frida_example.py
+++ dork/firda_example/frida_example.py
@@ -8,26 +8,23 @@ https://blog.csdn.net/Qwertyuiop2016/article/details/114284618
 """
 
 
+def read_text(fpath:str):
+    with open(fpath,"r") as f:
+        return f.read()
+script_enumerateModules=read_text("./script/enumerateModules.js")
+
 import frida
 import sys
 
-#device = frida.get_device_manager().enumerate_devices()
 local:core.Device = frida.get_local_device()
-# a = local.get_process('hello.exe')
-# print(a)
 pid:int = local.spawn("dork.exe")
-session = local.attach(pid)
-script = session.create_script("""
-var modules = Process.enumerateModules();
-for (let module of modules) {
-    send(module);
-}
-"""
+session:core.Session = local.attach(pid)
+script = session.create_script(script_enumerateModules )
 # % int('00000001400011D1', 16)
-                               )
-# start .text 00000001400011D1 00000005   R . . . . . .
 def on_message(message, data):
     print(message)
 script.on('message', on_message)
 script.load()
 sys.stdin.read()
+
+
