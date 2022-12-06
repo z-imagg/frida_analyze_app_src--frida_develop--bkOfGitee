08d0d53bddc5c41d5eb26612b559737ee3407a51
prgrmz01@163.com <prgrmz01@163.com>
2022-12-06 15:56:27 +0800

    清理无用，标出参考网页

--- dork/firda_example/frida_example.py
+++ dork/firda_example/frida_example.py
@@ -1,27 +1,13 @@
-import sys
 
 from frida import core
-from frida_tools.tracer import TracerApplication
-
-#让frida-trace能单独被调用运行
-# https://gitcode.net/fridaz/frida/-/commit/6ae5d35301c5d299a9a0f18077bb4a93801aef2c
-# https://gitcode.net/fridaz/frida-tools/-/commit/83cfbad68ec38fb98999ae50699c216b633dc9cd
-
-'''
-where frida-trace
-#D:/Python38/Scripts/frida-trace.exe
-
-frida-trace   -i "!"  -f  dork.exe
-'''
-# sys.argv=['D:\\Python38\\Scripts\\frida-trace.exe', '-i', '!', '-f', 'dork.exe']
-# sys.argv=['xxxx', '-i', '!', '-f', 'dork.exe']
-#
-# app = TracerApplication()
-# app.run()
 
 
+""" ref: 
+https://frida.re/docs/javascript-api/
+https://blog.csdn.net/Qwertyuiop2016/article/details/114284618
+"""
 
-# from __future__ import print_function
+
 import frida
 import sys
 
