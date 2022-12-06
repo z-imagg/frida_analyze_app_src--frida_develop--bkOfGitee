d4e47f598748eb73872758e91ac5e063b222a5bd
prgrmz01@163.com <prgrmz01@163.com>
2022-12-06 19:35:10 +0800

    demo

--- dork/firda_example/demo.py
+++ dork/firda_example/demo.py
@@ -42,12 +42,30 @@ class Application(object):
 var module = Process.findModuleByName('dork.exe');
 DebugSymbol.load("D:/instrmcpp/dork/firda_example/dork.exe");
 var mainFunc=DebugSymbol.getFunctionByName("main")
-
 Interceptor.attach(mainFunc, {
   onEnter: function (args) {
-    send(args);
+    send(args[0]);
   }
 });
+//message: pid=73552, payload={'type': 'send', 'payload': '0x1'}
+
+var funcAddrLs=DebugSymbol.findFunctionsMatching("*ZUser*");
+for (let funcAddr of funcAddrLs) {
+Interceptor.attach(funcAddr, {
+  onEnter: function (args) {
+    send(args[0]);
+  }
+});
+}
+/*
+⚡ message: pid=13460, payload={'type': 'send', 'payload': '0x5155cff9b8'}
+⚡ message: pid=13460, payload={'type': 'send', 'payload': '0x230ffcfb720'}
+⚡ message: pid=13460, payload={'type': 'send', 'payload': '0x230ffcfb720'}
+⚡ message: pid=13460, payload={'type': 'send', 'payload': '0x230ffcfb720'}
+⚡ message: pid=13460, payload={'type': 'send', 'payload': '0x5155cff9b8'}
+*/
+
+
 """)
         script.on("message", lambda message, data: self._reactor.schedule(lambda: self._on_message(pid, message)))
         print("✔ load()")
