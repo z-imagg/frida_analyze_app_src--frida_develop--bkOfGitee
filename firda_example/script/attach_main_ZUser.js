b88185abff9bce8a38e14b1a37a5af7b961d4cb9
prgrmz01@163.com <prgrmz01@163.com>
2022-12-06 23:47:59 +0800

    去掉重复的Interceptor.attach

--- firda_example/script/attach_main_ZUser.js
+++ firda_example/script/attach_main_ZUser.js
@@ -4,15 +4,10 @@ var module = Process.findModuleByName('dork.exe');
 //猜测: DebugSymbol.load 依赖 .exe文件 的 同目录 的 同名 .pdb文件
 DebugSymbol.load("D:/instrmcpp/dork/cmake-build-debug/dork.exe");
 var mainFunc=DebugSymbol.getFunctionByName("main")
-Interceptor.attach(mainFunc, {
-  onEnter: function (args) {
-    send(args[0]);
-  }
-});
 //message: pid=73552, payload={'type': 'send', 'payload': '0x1'}
 
 var funcAddrLs=DebugSymbol.findFunctionsMatching("*ZUser*");
-for (let funcAddr of funcAddrLs) {
+for (let funcAddr of [mainFunc,...funcAddrLs]) {
 Interceptor.attach(funcAddr, {
   onEnter: function (args) {
     send(args[0]);
