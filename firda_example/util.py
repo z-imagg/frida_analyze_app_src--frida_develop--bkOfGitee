5251bf3b5509ee3211a4eb8a5a456dace43fe835
prgrmz01@163.com <prgrmz01@163.com>
2022-12-08 18:42:12 +0800

    删掉无用代码

--- firda_example/util.py
+++ firda_example/util.py
@@ -6,16 +6,3 @@ class Util:
     def read_text(fpath:str):
         with open(fpath,"r",encoding='utf-8') as f:
             return f.read()
-
-    @staticmethod
-    def cmd_str_to_list(cmd_str:str)->List[str]:
-        if cmd_str is None: return None
-        lsRaw:List[str]=cmd_str.split(" ")
-        ls:List[str]=list(filter(lambda wordK:len(wordK.strip())>0,lsRaw))
-        return ls
-
-"""
-
-dork_run_cmd:str="D:/llvm-home/llvm-project/build/Debug/bin/clang.exe -S    -emit-llvm D:/instrmcpp/dork_simple/User.cpp"
-cmd_str_to_list(dork_run_cmd)
-"""
