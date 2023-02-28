21992fc211a06bef7e74cba02f0811218e95752f
prgrmz01@163.com <prgrmz01@163.com>
2023-02-28 16:51:07 +0800

    devvv

--- frida_main/attach_develop_wrap.sh
+++ frida_main/attach_develop_wrap.sh
@@ -6,10 +6,10 @@ loopCnt="500"
 fi
 
 for i in `seq 1 $loopCnt`; do
-bash -x attach_develop.sh 0  > /dev/null &
-pid=`cat ./py_pid`
-(sleep 4 ; kill -2  $pid; sleep 1 ;  kill -9  $pid;) &
-wait $pid ; sleep 2
+bash -x attach_develop.sh 0 
+py_pid=`cat ./py_pid`
+sleep 10 ; kill -2  $py_pid; sleep 1 ;  kill -9  $py_pid;
+# wait $py_pid ; sleep 2
 done
 
 #tail -f search.log
