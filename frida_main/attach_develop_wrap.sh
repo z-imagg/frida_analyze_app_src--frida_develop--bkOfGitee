f08172fe2bb032d0a0b83b57a98cdc67db09e773
caoguilin@enmonster.com <caoguilin@enmonster.com>
2023-02-28 18:53:33 +0800

    zzz

--- frida_main/attach_develop_wrap.sh
+++ frida_main/attach_develop_wrap.sh
@@ -6,9 +6,9 @@ loopCnt="500"
 fi
 
 for i in `seq 1 $loopCnt`; do
-bash -x attach_develop.sh 0 
+bash  attach_develop.sh 0 
 py_pid=`cat ./py_pid`
-sleep 10 ; kill -2  $py_pid; sleep 1 ;  kill -9  $py_pid;
+sleep  3; kill -2  $py_pid;   kill -9  $py_pid;
 # wait $py_pid ; sleep 2
 done
 
