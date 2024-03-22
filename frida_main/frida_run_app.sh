76e8bad20b6ebcc4c5322b2314644221ce653352
prgrmz07 <prgrmz07@163.com>
2024-03-22 18:22:27 +0800

    py的frida脚本依然没让Stalker.follow正常
    http://giteaz:3000/frida_analyze_app_src/frida_js/src/commit/b30e4e2cbf92a80f1abe92f7d1f1dc410ccabb6e/frida-trace.js

--- frida_main/frida_run_app.sh
+++ frida_main/frida_run_app.sh
@@ -1,42 +1,11 @@
 #!/bin/sh
 
 
-argVal=$2
-if [[ "X$argVal" == "X" ]]; then
-argVal="define"
-fi
+echo -n ' ' > _args.txt
 
+#http://giteaz:3000/frida_analyze_app_src/frida_js/src/commit/b30e4e2cbf92a80f1abe92f7d1f1dc410ccabb6e/frida-trace.js
 
-echo '''
-#include <stdio.h>
-#define MAX_USER 256734
-#define ARRIVE_MAX (MAX_USER>5)
-int main(int argc, char **argv )
-{
-int i=ARRIVE_MAX*965732;
-for(i=0; i< argc; i++){
-printf("argi:%d,%s\n",i,argv[i]);
-}
-return 0;
-}
-''' > echo_args.c
-
-#clang  -c  echo_args.c
-#clang echo_args.o -o echo_args
-
-echo -n '-c  echo_args.c -v' > clang_args.txt
-#echo -n '_nothing,' > _funcNameLsIgnore_.txt
-touch _funcNameLsIgnore_.txt
-echo -n 'frida-,libpthread-,linux-vdso' > _moduleNamePrefixListIgnore_.txt
-
-# echo -n "$argIdx" > _argSearch_argIndex_.txt
-echo -n "$argVal" > _argSearch_target_.txt
-# touch ${argIdx}_argSearch_result.txt
-
-
-
-clang_real_path=$(readlink  -f `which clang`)
-python frida_run_app.py $clang_real_path   clang_args.txt  `pwd`  frida_js/develop.js _funcNameLsIgnore_.txt  2>&1   | tee -a search.log
+python frida_run_app.py /fridaAnlzAp/torch-cpp/v1.0.0/simple_nn.elf   _args.txt  `pwd`  /fridaAnlzAp/frida_js/frida-trace.js _funcNameLsIgnore_.txt  2>&1   | tee -a search.log
 #python frida_run_app.py c:/Windows/notepad.exe  d:/1.txt  /pubx/instrmcpp/frida-agent-4instrmcpp/attach_operator_new__constructor.js _funcNameLsIgnore_.txt
 pid=$!
 echo -n $pid > ./py_pid
