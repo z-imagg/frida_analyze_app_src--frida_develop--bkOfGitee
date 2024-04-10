f76e11e3281dbebbdadba9c18938b126d21b0ecc
prgrmz07 <prgrmz07@163.com>
2024-04-10 20:14:05 +0800

    没执行脚本，ConsoleApplication并不能直接使用，还是去改应用testdisk为非交互式吧
    https://gitee.com/frida_analyze_app_src/frida--frida-tools/commit/672dc7ab3297a9d177873d38d4a90e650d9c5e8c

--- frida_run_app.py
+++ frida_run_app.py
@@ -12,7 +12,7 @@ from __future__ import print_function
 from argparse import Namespace
 import typing
 from pathlib import Path
-from typing import List
+from typing import Callable, List
 
 import frida
 import frida_tools
@@ -20,17 +20,37 @@ import threading
 
 from frida_tools.repl import main
 from frida_tools.reactor import Reactor
-from frida_tools.application import ConsoleApplication
+from frida_tools.application import ConsoleApplication, await_enter
 
 import sys
 
 
-_options=Namespace(device_id=None,   target=('file', ['/fridaAnlzAp/cgsecurity--testdisk/src/testdisk']),    args=['/fridaAnlzAp/cgsecurity--testdisk/hd.imgxx'],  user_scripts=['/fridaAnlzAp/frida_js/InterceptFnSym.js'],     logfile='1.log', )
+_options=Namespace(device_id=None, device_type=None, host=None, certificate=None, origin=None, token=None, keepalive_interval=None, session_transport='multiplexed', stun_server=None, relays=None, target=('file', ['/fridaAnlzAp/cgsecurity--testdisk/src/testdisk']), stdio='inherit', aux=[], realm='native', runtime=None, enable_debugger=False, squelch_crash=False, args=['/fridaAnlzAp/cgsecurity--testdisk/hd.imgxx'], options_file=None, user_scripts=['/fridaAnlzAp/frida_js/InterceptFnSym.js'], user_parameters=None, user_cmodule=None, toolchain='any', codeshare_uri=None, eval_items=None, quiet=False, timeout=0, on_spawn_complete='resume', logfile='1.log', eternalize=False, exit_on_error=False, autoperform=False, autoreload=True)
+
+class MyConsoleApp(ConsoleApplication):
+    def __init__(self, 
+        run_until_return: Callable[[Reactor], None] = ..., 
+        on_stop: Callable[[], None] | None = None, 
+        args: List[str] | None = None,
+        _options:Namespace=None
+        ):
+        super().__init__(
+            run_until_return=run_until_return,
+            on_stop=on_stop, 
+            args=args,
+            _options=_options
+            )
+        
+    def _needs_target(self) -> bool:
+        return True
+
+
 _stop_requested:threading.Event = threading.Event()
-app=ConsoleApplication(
+app=MyConsoleApp(
     run_until_return=lambda reactor: _stop_requested.wait(),
     # on_stop=lambda x: _stop_requested.set()
     _options=_options
 )
+
 app._aux=["cwd=(string)/tmp/"]
 app.run()
